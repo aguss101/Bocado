9720efff33893996fd50ff89aa42c91181142e0b
Agustin <agustinbaied1@gmail.com>
2026-03-09 22:39:36 -0300

    Improve Querys

--- Desarrollo/app/src/main/java/com/example/bocado/Estaticos/Query.java
+++ Desarrollo/app/src/main/java/com/example/bocado/Estaticos/Query.java
@@ -1,45 +1,48 @@
 package com.example.bocado.Estaticos;
 public class Query {
     public static String getUsers(Integer userID){
-        String query = "SELECT " +
-                "\"Usuarios\".id as id, " +
-                "\"Cuentas\".nombre as cuenta, " +
-                "\"Naciones\".nombre as pais, " +
-                "\"Generos\".nombre as genero, " +
-                "\"Usuarios\".nombre, \"Usuarios\".apellido, " +
-                "\"Usuarios\".correo, \"Usuarios\".usuario, \"Usuarios\".contrasena, " +
-                "\"Usuarios\".fecha_nacimiento, \"Usuarios\".fecha_creacion, \"Usuarios\".fecha_acceso, " +
-                "\"Usuarios\".activo, \"Usuarios\".visibilidad, \"Usuarios\".foto, \"Usuarios\".banner " +
-                "FROM \"Usuarios\" " +
-                "JOIN \"Cuentas\" ON \"Usuarios\".id_cuenta = \"Cuentas\".id " +
-                "JOIN \"Naciones\" ON \"Usuarios\".id_nacion = \"Naciones\".id " +
-                "JOIN \"Generos\" ON \"Usuarios\".id_genero = \"Generos\".id";
+        String query = """
+                SELECT
+                    usuarios.id AS id,
+                    cuentas.nombre AS cuenta,
+                    naciones.nombre AS pais,
+                    generos.nombre AS genero,
+                    usuarios.nombre, usuarios.apellido,
+                    usuarios.correo, usuarios.usuario, usuarios.contrasena,
+                    usuarios.fecha_nacimiento, usuarios.fecha_creacion, usuarios.fecha_acceso,
+                    usuarios.activo, usuarios.visibilidad, usuarios.foto, usuarios.banner
+                FROM usuarios
+                JOIN cuentas ON usuarios.id_cuenta = cuentas.id
+                JOIN naciones ON usuarios.id_nacion = naciones.id
+                JOIN generos ON usuarios.id_genero = generos.id
+        """;
         if(userID != null){
-            query += " WHERE \"Usuarios\".id = " + userID;
+            query += " WHERE usuarios.id = " + userID;
         }
         return query;
     }
     public static String getFoods(Integer foodID){
-        String query = "SELECT " +
-                "\"Alimentos\".id AS id, " +
-                "\"Alimentos\".nombre, " +
-                "\"Usuarios\".usuario AS usuario, " +
+        String query = """
+                SELECT
+                alimentos.id AS id,
+                alimentos.nombre,
+                usuarios.usuario AS usuario,
 
-                "MAX(CASE WHEN \"Nutrientes\".nombre = 'Proteinas' THEN \"Alimentos_Nutrientes\".valor100gr END) AS proteinas, " +
-                "MAX(CASE WHEN \"Nutrientes\".nombre = 'Carbohidratos' THEN \"Alimentos_Nutrientes\".valor100gr END) AS carbohidratos, " +
-                "MAX(CASE WHEN \"Nutrientes\".nombre = 'Grasas' THEN \"Alimentos_Nutrientes\".valor100gr END) AS grasas, " +
-                "MAX(CASE WHEN \"Nutrientes\".nombre = 'Vitamina C' THEN \"Alimentos_Nutrientes\".valor100gr END) AS vitamina_c " +
+                MAX(CASE WHEN nutrientes.nombre = 'Proteinas' THEN alimentos_nutrientes.valor100gr END) AS proteinas,
+                MAX(CASE WHEN nutrientes.nombre = 'Carbohidratos' THEN alimentos_nutrientes.valor100gr END) AS carbohidratos,
+                MAX(CASE WHEN nutrientes.nombre = 'Grasas' THEN alimentos_nutrientes.valor100gr END) AS grasas,
+                MAX(CASE WHEN nutrientes.nombre = 'Vitamina C' THEN alimentos_nutrientes.valor100gr END) AS vitamina_c
 
-                "FROM \"Alimentos\" " +
-                "LEFT JOIN \"Usuarios\" ON \"Usuarios\".id = \"Alimentos\".id_usuario " +
-                "LEFT JOIN \"Alimentos_Nutrientes\" ON \"Alimentos_Nutrientes\".id_alimento = \"Alimentos\".id " +
-                "LEFT JOIN \"Nutrientes\" ON \"Nutrientes\".id = \"Alimentos_Nutrientes\".id_nutriente ";
-
+                FROM alimentos
+                LEFT JOIN usuarios ON usuarios.id = alimentos.id_usuario
+                LEFT JOIN alimentos_nutrientes ON alimentos_nutrientes.id_alimento = alimentos.id
+                LEFT JOIN nutrientes ON nutrientes.id = alimentos_nutrientes.id_nutriente
+            """;
         if(foodID != null){
-            query += "WHERE \"Alimentos\".id = " + foodID + " ";
+            query += "WHERE alimentos.id = " + foodID + " ";
         }
 
-        query += "GROUP BY \"Alimentos\".id, \"Alimentos\".nombre, \"Usuarios\".usuario";
+        query += "GROUP BY alimentos.id, alimentos.nombre, usuarios.usuario";
 
         return query;
     }
